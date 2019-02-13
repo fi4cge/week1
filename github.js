4c3f62d6a6e2c9463df8d1dbff3dea06dc90394f
fi4cge <47338291+fi4cge@users.noreply.github.com>
2019-02-13 17:54:18 -0500

    Add files via upload

---
+++ github.js
@@ -0,0 +1,34 @@
+/*
+Exercise File for Week 1 Source, see Week1.txt for details
+*/
+
+/*
+These are functions, but why the weird syntax?
+*/
+const personOne = favoriteFoods => {
+  var foods = ["pizza", "tacos", "donuts", "sandwich"];
+  //var arrayLength = foods.length;
+  for (var i = 0; i < arrayLength; i++) {
+    console.log(foods[i]);
+};
+
+const personTwo = favoriteMovies => {
+  /*insert functionality here*/
+};
+
+const personThree = favoriteAnimals => {
+  /*insert functionality here*/
+};
+
+/*
+What does this variable syntax do?
+*/
+const favoriteFoods = [],
+  favoriteMovies = [],
+  favoriteAnimals = [];
+
+const iceBreaker = () => {
+  /*
+    Call each function here
+    */
+};
